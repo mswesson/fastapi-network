9a13a565a3ad7f692c8bcf2daea9880d3a54cfda
Eduard Komarov <rango.adik@gmail.com>
2024-10-02 18:53:16 +0800

    Update database.py

--- app/src/database.py
+++ app/src/database.py
@@ -3,8 +3,6 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 from config import DATABASE_URL
 
-# DATABASE_URL = "postgresql+asyncpg://mswesson:386790@db/twitter_on_fastapi"
-
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
